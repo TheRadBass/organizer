adda8f1d95fc7e3e8aa04d5bb75b331af2312771
Martin Bracamonte <maabte@gmail.com>
2023-04-09 16:15:22 -0700

    Added logic for handleSubmit validation and show error info

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -5,8 +5,9 @@ import { v4 as uuidv4 } from "uuid";
 const ToDoList = () => {
   const [toDos, setToDos] = useState<ToDo[]>([]);
   const [task, setTask] = useState<string>("");
-  const [priority, setPriority] = useState<Priority>();
-  const [errorMessage, setErrorMessage] = useState("");
+  const [priority, setPriority] = useState<Priority>("Low");
+  const [date, setDate] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState<string>("");
 
   const handleTask = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTask(e.target.value);
@@ -27,33 +28,73 @@ const ToDoList = () => {
     }
   };
 
+  const handleDueDate = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value !== "") {
+      setDate(e.target.value);
+    }
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (task !== "" && date !== "") {
+      let currentTodos = toDos;
+      currentTodos.push({ task: task, priority: priority, dueDate: date });
+      setToDos(currentTodos);
+      cleanNewToDo();
+    } else {
+      setErrorMessage("All fields must be filled");
+    }
+  };
+
+  const cleanNewToDo = () => {
+    setTask("");
+    setPriority("Low");
+    setDate("");
+  };
+
   return (
     <>
+      {errorMessage}
       <form className="new-to-do-form">
         <label>
           Task:
           <input
+            value={task}
             onChange={handleTask}
             type="text"
             id="task"
             name="task"
             maxLength={100}
+            required
           ></input>
         </label>
         <label>
           Priority:
-          <select onChange={handlePriority} name="priority" id="priority">
+          <select
+            value={priority}
+            onChange={handlePriority}
+            name="priority"
+            id="priority"
+            required
+          >
             <option value={"Low"}>Low</option>
             <option value={"Medium"}>Medium</option>
             <option value={"High"}>High</option>
           </select>
         </label>
         <label>
-          {errorMessage}
           Due:
-          <input type="date" name="date" id="date"></input>
+          <input
+            value={date}
+            onChange={handleDueDate}
+            type="date"
+            name="date"
+            id="date"
+          ></input>
         </label>
-        <button type="submit">Add to-do</button>
+        <button onClick={handleSubmit} type="submit">
+          Add to-do
+        </button>
       </form>
       <div className="to-dos-wrapper">
         {toDos.map((toDo) => (
