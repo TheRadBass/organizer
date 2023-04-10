171c3efdd1fdb639a27916fceaa6f236a2a2edb3
Martin Bracamonte <maabte@gmail.com>
2023-04-10 16:19:35 -0700

    Deleted unnecessary paragraphs

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ const App = () => {
     <>
       <div className="wrapper">
         <nav className="main_nav_bar">This is just a navBar</nav>
-        <div>This is the home page</div>
-        <p>Not much to see here yet</p>
       </div>
       <ToDoList />
     </>
